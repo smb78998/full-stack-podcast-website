1136012c9723a12f4d003e8e7684a5053d021dc2
smb7898 <smb78998@gmail.com>
2024-02-13 22:13:54 -0500

    add animations to title

--- scripts.js
+++ scripts.js
@@ -23,12 +23,19 @@ document.querySelectorAll(".nav__list").forEach(n => n.addEventListener("click",
 
 
 //**********RESPONSIVE COMMUNITY************** */
+//would be best to name the section "carousel" becuase this would turn all carousel items into responsiveslider
+
+
 document.querySelectorAll(".community__feedback").forEach((community__feedback) => {
+
+  //makes an "array" for each of the community feedback boxes
   const items = community__feedback.querySelectorAll(".community__feedback__box");
+  //creates buttons for each element in our array 
   const buttonsHtml = Array.from(items, () => {
     return `<span class="carousel__button"></span>`;
   });
 
+  //before the end of our .community__feedback section we are adding in the buttons 
   community__feedback.insertAdjacentHTML(
     "beforeend",
     `
@@ -42,8 +49,8 @@ document.querySelectorAll(".community__feedback").forEach((community__feedback) 
 
   buttons.forEach((button, i) => {
     button.addEventListener("click", () => {
-      
-      // unselect
+
+      // remove the class "selected" from all of the community__feedback__box (and the button too)
       items.forEach((item) =>
         item.classList.remove("community__feedback__box--selected")
       );
@@ -51,12 +58,13 @@ document.querySelectorAll(".community__feedback").forEach((community__feedback) 
         button.classList.remove("carousel__button--selected")
       );
 
+      //add the "selected" class 
       items[i].classList.add("community__feedback__box--selected");
       button.classList.add("carousel__button--selected");
     });
   });
 
-  //by default select first one
+  //by default select first one, for when the page first loads 
   items[0].classList.add("community__feedback__box--selected");
   buttons[0].classList.add("carousel__button--selected");
 });
